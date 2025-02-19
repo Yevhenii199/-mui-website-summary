90418db276a501c881e3cfabf083a9341eaa451b
Yevhenii199 <gshijanov5@gmail.com>
2025-02-19 20:57:56 +0100

    Grouped code

--- src/pages/Todo.test.js
+++ src/pages/Todo.test.js
@@ -11,48 +11,35 @@ describe("Todo App", () => {
   test("2️⃣ Поле для введення підтримує цифри та букви", () => {
     render(<Todo />);
     const input = screen.getByLabelText(/Нова задача/i);
-
     fireEvent.change(input, { target: { value: "Задача 123" } });
     expect(input.value).toBe("Задача 123");
   });
 
   test("3️⃣ Порожній ввід показує помилку", () => {
-    render(<Todo />);
-    
+    render(<Todo />);  
     const button = screen.getByText(/Додати/i);
     fireEvent.click(button);
-    
     expect(screen.getByText(/Поле не може бути порожнім!/i)).toBeInTheDocument();
   });
 
-
-test("4️⃣ Додавання нової задачі у список", () => {
-  render(<Todo />);
-    
-  const input = screen.getByLabelText(/Нова задача/i);
-  const button = screen.getByText(/Додати/i);
-
-  fireEvent.change(input, { target: { value: "Завдання для тесту" } });
-  fireEvent.click(button);
-
-  const checkbox = screen.getByRole("checkbox");
-  expect(checkbox.checked).toBe(false);
-
-  fireEvent.click(checkbox);
-  expect(checkbox.checked).toBe(true);
-});
-
-
-
+  test("4️⃣ Додавання нової задачі у список", () => {
+    render(<Todo />);   
+    const input = screen.getByLabelText(/Нова задача/i);
+    const button = screen.getByText(/Додати/i);
+    fireEvent.change(input, { target: { value: "Завдання для тесту" } });
+    fireEvent.click(button);
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox.checked).toBe(false);
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toBe(true);
+  });
 
   test("5️⃣ Введення нового тексту очищає поле вводу після додавання", () => {
     render(<Todo />);
     const input = screen.getByLabelText(/Нова задача/i);
     const button = screen.getByText(/Додати/i);
-
     fireEvent.change(input, { target: { value: "Ще одна задача" } });
     fireEvent.click(button);
-
     expect(input.value).toBe("");
   });
 });
